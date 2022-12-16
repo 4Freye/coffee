237e87868a08bacffc8a22edf3c44c9e4cac04b9
4Freye <4freye@gmail.com>
2022-12-16 14:54:26 +0100

    created functions to convert a series to a set of dummy variables

--- CoffeeLibrary/FeatureCreate.py
+++ CoffeeLibrary/FeatureCreate.py
@@ -1,1 +1,54 @@
+#import packages
+import pandas as pd
+from collections import Counter
+from nltk.corpus import stopwords
+import nltk
+import re
+from itertools import islice
 
+#import stopwords
+nltk.download('stopwords')
+
+class FeatureCreate:
+    def __init__(self, df, series):
+        self.df = df
+        self.series = self.df[series]
+        
+        #create list of words 
+        self.corpus =  ' '.join(self.series).replace('.', ' ').replace(';', ' ').replace(',', ' ')
+
+        # split() returns list of all the words in the string
+        self.corpus_list = corpus.split()
+
+        #words that aren't useful
+        stop_words = set(stopwords.words('english'))
+        stop_words.add('cup')
+
+        #filter- this gets us only the interesting ones
+        self.corpus_list = [w for w in self.corpus_list if not w.lower() in stop_words]
+
+
+    def top_n_unigram(self, n = 20):
+        # Python program to find the n most frequent words. then return dummy variables if frequent word is in series
+
+        # Pass the list to instance of Counter class.
+        top_n = Counter(corpus_list).most_common(n)
+        #just get the words
+        top_words = [tup[0] for tup in top_n]
+        #convert to series
+        top_words = pd.Series(top_words)
+        #use apply to see if it's in description
+        return top_words.apply(lambda x: self.series.str.contains(x, case=False)).set_index(top_words).transpose()
+        
+    def top_n_bigram(self, n = 10):
+        # Python program to find the n most frequent bigrams. then return dummy variables if frequent bigram is in series
+
+        # Pass the list to instance of Counter class.
+        top_n =  Counter(zip(corpus_list, islice(corpus_list, 1, None))).most_common(n)
+
+        #just get the words
+        top_words = [tup[0] for tup in top_n]
+        #convert to series
+        top_words = pd.Series(top_words).apply(lambda x: ' '.join(x))
+        #use apply to see if it's in description
+        return top_words.apply(lambda x: self.series.str.contains(x, case=False)).set_index(top_words).transpose()
